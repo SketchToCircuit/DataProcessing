9875c968154487fa299d89598c688758395fa311
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-11-17 14:49:24 +0100

    fix import as __main__

--- CircuitSynthesis/PinDetection/pindetection.py
+++ CircuitSynthesis/PinDetection/pindetection.py
@@ -11,9 +11,14 @@ import numpy as np
 from scipy import ndimage
 from scipy.ndimage.measurements import label
 
-import PinDetection.image_manipulation as imgman
-import PinDetection.linedetection as ld
-from .match_pins import *
+if __name__ == '__main__':
+    import image_manipulation as imgman
+    import linedetection as ld
+    from match_pins import *
+else:
+    import PinDetection.image_manipulation as imgman
+    import PinDetection.linedetection as ld
+    from .match_pins import *
 
 @dataclass
 class Component:
