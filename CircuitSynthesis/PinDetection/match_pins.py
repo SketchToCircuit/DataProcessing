9a6a9df3f3e84f59ea3b70549953d5164717ee4b
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-11-17 14:23:30 +0100

    initial pindetection data export

---
+++ CircuitSynthesis/PinDetection/match_pins.py
@@ -0,0 +1,329 @@
+import math
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Dict, List
+
+import cv2
+import numpy as np
+
+# information of one pin
+@dataclass
+class Pin:
+    name: str
+    '''Name of the pin (eg. "gate", "1", "out", ...)'''
+    position: np.ndarray
+    '''Position of the pin as 1D numpy array with size 2: [x, y]'''
+    direction: np.ndarray
+    '''Direction of the pin as 1D numpy array with size 2: [x_dir, y_dir]'''
+
+    def to_dict(self):
+        '''Generate a dict object to export as JSON.'''
+        return {'name': self.name, 'position': self.position.tolist(), 'direction': self.direction.tolist()}
+    
+    @staticmethod
+    def from_dict(map):
+        '''Generate a class instance from a dict object.'''
+        try:
+            return Pin(map['name'], np.array(map['position']), np.array(map['direction']))
+        except Exception:
+            return Pin('0', np.zeros(2), np.zeros(2))
+
+# interface for pin detector
+class IPinDetection(ABC):
+    @property
+    @classmethod
+    @abstractmethod
+    def _COMP_TYPES(cls): raise NotImplementedError
+
+    @property
+    @classmethod
+    @abstractmethod
+    def NAME(cls): raise NotImplementedError
+
+    @classmethod
+    def match(cls, component_type: str) -> bool :
+        return component_type in cls._COMP_TYPES
+    
+    @staticmethod
+    @abstractmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img: np.ndarray) -> Dict[str, Pin]:
+        raise NotImplementedError
+
+class TwoPinsHor(IPinDetection):
+    _COMP_TYPES = ['R', 'C', 'L', 'R_H', 'C_H', 'L_H',
+    'LED', 'D', 'S1', 'S2', 'BTN1', 'BTN2', 'V_H', 'A_H', 'U_AC_H',
+    'LMP', 'M', 'F', 'D_Z', 'D_S', 'C_P']
+    NAME = 'TwoPinsHorizontale'
+
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # calculate dx and dy
+        deltas = np.diff(lines, axis=-2).squeeze(axis=-2)
+        # create mask to remove elements where dy > dx
+        mask = (np.diff(np.abs(deltas)) < 0).squeeze(axis=-1)
+        lines = lines[mask, :, :]
+
+        # get points with minimum and maximum of: x - abs(y - cy) or x + abs(y - cy) to take lines near to vertical center
+        values = lines[:, :, 0] + np.abs(lines[:, :, 1] - centroid[1])
+        left_point = lines[np.unravel_index(np.argmin(values), values.shape[0:2])]
+        values = lines[:, :, 0] - np.abs(lines[:, :, 1] - centroid[1])
+        right_point = lines[np.unravel_index(np.argmax(values), values.shape[0:2])]
+
+        left_point[0] = left_point[0] + img_size[1] / 30
+        right_point[0] = right_point[0] - img_size[1] / 30
+        
+        return {'1': Pin('1', left_point, np.array([-1, 0])), '2': Pin('2', right_point, np.array([1, 0]))}
+
+class TwoPinsVert(IPinDetection):
+    _COMP_TYPES = ['R_V', 'C_V', 'L_V',
+    'V_V', 'A_V', 'U1', 'U2', 'I1', 'I2', 'U3', 'BAT', 'U_AC_V', 'M_V']
+    NAME = 'TwoPinsVertical'
+    
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # rotate and then use other class
+        rot_lines = lines[:, :, ::-1] * [1, -1]
+        rot_centroid = centroid[::-1] * [1, -1]
+
+        pins = TwoPinsHor.get_pins(rot_lines, rot_centroid, img_size[::-1])
+
+        for pin in pins.values():
+            pin.position = (pin.position * [1, -1])[::-1]
+            pin.direction = (pin.direction * [1, -1])[::-1]
+
+        return pins
+
+class OnePinLeft(IPinDetection):
+    _COMP_TYPES = ['PIN']
+    NAME = 'OnePinLeft'
+    
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # filter vertical lines with dx and dy
+        deltas = np.diff(lines, axis=-2).squeeze(axis=-2)
+        mask = (np.diff(np.abs(deltas)) < 0).squeeze(axis=-1)
+        lines = lines[mask, :, :]
+
+        # get point with minimum of: x + abs(y - cy) to take lines near to vertical center
+        values = lines[:, :, 0] + np.abs(lines[:, :, 1] - centroid[1])
+        left_point = lines[np.unravel_index(np.argmin(values), values.shape[0:2])]
+
+        left_point[0] = left_point[0] + img_size[1] / 30
+        
+        return {'1': Pin('1', left_point, np.array([-1, 0]))}
+
+class OnePinTop(IPinDetection):
+    _COMP_TYPES = ['GND', 'GND_F', 'GND_C']
+    NAME = 'OnePinTop'
+    
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # rotate and use other class
+        rot_lines = lines[:, :, ::-1] * [1, -1]
+        rot_centroid = centroid[::-1] * [1, -1]
+
+        pin = OnePinLeft.get_pins(rot_lines, rot_centroid, img_size[::-1])['1']
+
+        pin.position = (pin.position * [1, -1])[::-1]
+        pin.direction = np.array([0, -1])
+
+        return {'1': pin}
+
+class TwoPinsLeft(IPinDetection):
+    _COMP_TYPES = ['SPK', 'MIC']
+    NAME = 'TwoPinsLeft'
+    
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # filter vertical lines
+        deltas = np.diff(lines, axis=-2).squeeze(axis=-2)
+        mask = (np.diff(np.abs(deltas)) < 0).squeeze(axis=-1)
+        lines = lines[mask, :, :]
+
+        # get first point (farthest to the left) with x + abs(y -cy) as metric
+        values = lines[:, :, 0] + np.abs(lines[:, :, 1] - centroid[1])
+        left_1_idx = np.unravel_index(np.argmin(values), values.shape[0:2])
+        left_1 = lines[left_1_idx]
+
+        # calculate verrtical bounding area around first used line with margin
+        min_y, max_y = np.sort(lines[left_1_idx[0]][:, 1])
+        min_y -= img_size[1] / 50
+        max_y += img_size[1] / 50
+
+        # remove already used line
+        lines = np.delete(lines, left_1_idx[0], axis=0)
+
+        # create mask for all lines where both endpoints are outside of the bounding rect
+        mask = np.logical_or(lines[:, :, 1] < min_y , lines[:, :, 1] > max_y)
+        mask = np.logical_and(mask[:, 0], mask[:, 1])
+        lines = lines[mask, :, :]
+
+        # get second point
+        values = lines[:, :, 0] + np.abs(lines[:, :, 1] - centroid[1])
+        left_2_idx = np.unravel_index(np.argmin(values), values.shape[0:2])
+        left_2 = lines[left_2_idx]
+
+        left_1[0] = left_1[0] + img_size[1] / 30
+        left_2[0] = left_2[0] + img_size[1] / 30
+
+        return {'1': Pin('1', left_1, np.array([-1, 0])), '2': Pin('2', left_2, np.array([-1, 0]))}
+
+class TwoLOneRHor(IPinDetection):
+    _COMP_TYPES = ['OPV']
+    NAME = 'TwoLeftOneRight'
+    
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # flipped lines: image size - coordinate (correct y coordinate afterwards)
+        lines_flip = (img_size[::-1] * [1, 0] - lines[:, :]) * [1, -1]
+        # flip centroid
+        centroid_flip = [img_size[1] - centroid[0], centroid[1]]
+
+        inputs = [*TwoPinsLeft.get_pins(lines, centroid, img_size).values()]
+        out = OnePinLeft.get_pins(lines_flip, centroid_flip, img_size)['1']
+
+        # flip right pin back and rename
+        out.position[0] = img_size[1] - out.position[0]
+        out.direction = np.array([1, 0])
+        out.name = 'out'
+
+        # first input is higher -> negative
+        if inputs[0].position[1] < inputs[1].position[1]:
+            inputs[0].name = '-'
+            inputs[1].name = '+'
+
+            return {'+': inputs[1], '-': inputs[0], 'out': out}
+        else:
+            inputs[0].name = '+'
+            inputs[1].name = '-'
+            return {'+': inputs[0], '-': inputs[1], 'out': out}
+
+class TwoROneLHor(IPinDetection):
+    _COMP_TYPES = ['S3']
+    NAME = 'TwoRightOneLeft'
+    
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # flip img and lines
+        lines_flip = lines
+        lines_flip = (img_size[::-1] * [1, 0] - lines[:, :]) * [1, -1]
+        centroid_flip = [img_size[1] - centroid[0], centroid[1]]
+
+        pins = TwoLOneRHor.get_pins(lines_flip, centroid_flip, img_size)
+
+        # flip back
+        for pin in pins.values():
+            pin.position[0] = img_size[1] - pin.position[0]
+            pin.direction = pin.direction * [-1, 0]
+        
+        return pins
+
+class Potentiometer(IPinDetection):
+    _COMP_TYPES = ['POT']
+    NAME = 'Potentiometer'
+
+    @staticmethod
+    def _get_pin_diagonal_bottom(lines: np.ndarray, centroid: np.ndarray, angles: np.ndarray, img_size: np.ndarray):
+        # create mask for elements where angle in [10°; 90°]
+        mask = np.abs(angles * 180 / math.pi - 50) < 41
+        lines = lines[mask, :, :]
+
+        # get point with maximum of: 2 * y - abs(x - cx) to take lines near to horizontale center
+        values = 2 * lines[:, :, 1] - np.abs(lines[:, :, 0] - centroid[0])
+        bottom_point = lines[np.unravel_index(np.argmax(values), values.shape[0:2])]
+
+        bottom_point[1] = bottom_point[1] - img_size[0] / 30
+        bottom_point[0] = bottom_point[0] + img_size[0] / 30
+        
+        return Pin('3', bottom_point, np.array([0, 1]))
+
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        # get left and right pins
+        pins = TwoPinsHor.get_pins(lines, centroid, img_size)
+
+        # find diagonal lines
+        deltas = np.abs(np.diff(lines, axis=-2).squeeze(axis=-2))
+
+        # create mask for elements where atan2(dy, dx) in [15°; 80°]
+        angles = np.arctan2(deltas[:, 1], deltas[:, 0])
+        mask = np.abs(angles * 180 / math.pi - 47.5) < 33
+        diagonal_lines = lines[mask, :, :]
+
+        # dx * dy > 0 -> correct quadrant of diagonal
+        deltas = np.diff(diagonal_lines, axis=-2).squeeze(axis=-2)
+        mask = (deltas[:, 0] * deltas[:, 1]) < 0
+        diagonal_lines = diagonal_lines[mask, :, :]
+
+        # find lines where one point is above and one is below the center and one is left and one right
+        below_c = (diagonal_lines[:, :, 1] - centroid[1]) * 1.5 > 0
+        mask = np.logical_xor(below_c[:, 0], below_c[:, 1])
+        diagonal_lines = diagonal_lines[mask, :, :]
+
+        left_c = diagonal_lines[:, :, 0] < centroid[0]
+        mask = np.logical_xor(left_c[:, 0], left_c[:, 1])
+        diagonal_lines = diagonal_lines[mask, :, :]
+
+        # mask for lines near horizontale center
+        mask = np.abs((diagonal_lines[:, 0, 0] + diagonal_lines[:, 1, 0]) / 2 - centroid[0]) < img_size[1] / 5
+
+        if np.count_nonzero(mask) > 0:
+            # diagonal line in the middle -> pin on bottom
+            # shift centroid to the left and flip vertically
+            centroid[0] = centroid[0] - img_size[1] / 15
+
+            pins['3'] = Potentiometer._get_pin_diagonal_bottom(lines, centroid, angles, img_size)
+        else:
+            # pin on top
+            pins['3'] = OnePinTop.get_pins(lines, centroid, img_size)['1']
+            pins['3'].name = '3'
+
+        return pins
+
+class TransistorFET(IPinDetection):
+    _COMP_TYPES = ['JFET_N', 'JFET_P', 'MFET_N_D', 'MFET_N_E',
+    'MFET_P_D', 'MFET_P_E']
+    NAME = 'TransistorFET'
+
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        pin_points = _Transistor.get_pin_points(lines, centroid, img_size)
+        return {'gate': Pin('gate', pin_points['l'], np.array([-1, 0])), 'drain': Pin('drain', pin_points['tr'], np.array([0, -1])), 'source': Pin('source', pin_points['br'], np.array([0, 1]))}
+
+class TransistorBipolar(IPinDetection):
+    _COMP_TYPES = ['NPN', 'PNP']
+    NAME = 'TransistorBipolar'
+
+    @staticmethod
+    def get_pins(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        pin_points = _Transistor.get_pin_points(lines, centroid, img_size)
+        return {'base': Pin('base', pin_points['l'], np.array([-1, 0])), 'collector': Pin('collector', pin_points['tr'], np.array([0, -1])), 'emitter': Pin('emitter', pin_points['br'], np.array([0, 1]))}
+
+class _Transistor:
+    @staticmethod
+    def get_pin_points(lines: np.ndarray, centroid: np.ndarray, img_size: np.ndarray):
+        left = OnePinLeft.get_pins(lines, centroid, img_size)['1']
+
+        # filter horizontale lines
+        deltas = np.abs(np.diff(lines, axis=-2).squeeze(axis=-2))
+        # create mask for elements where atan2(dy, dx) in [20°; 90°]
+        angles = np.arctan2(deltas[:, 1], deltas[:, 0])
+        mask = np.abs(angles * 180 / math.pi - 55) < 36
+        lines = lines[mask, :, :]
+
+        # move centroid to the right to center for the right pins
+        centroid[0] += img_size[1] / 4
+
+        # get point with minimum of: y + abs(x - cx) to take lines near to horizontale center
+        values = lines[:, :, 1] + np.abs(lines[:, :, 0] - centroid[0])
+        top_point = lines[np.unravel_index(np.argmin(values), values.shape[0:2])]
+        # get point with maximum of: y - abs(x - cx) to take lines near to horizontale center
+        values = lines[:, :, 1] - np.abs(lines[:, :, 0] - centroid[0])
+        bottom_point = lines[np.unravel_index(np.argmax(values), values.shape[0:2])]
+
+        top_point[1] = top_point[1] + img_size[0] / 30
+        bottom_point[1] = bottom_point[1] - img_size[0] / 30
+
+        return {'l': left.position, 'tr': top_point, 'br': bottom_point}
+
+ALL_DETECTORS: List[IPinDetection] = [TwoPinsHor, TwoPinsVert, OnePinLeft, OnePinTop, TwoPinsLeft, TwoLOneRHor, TwoROneLHor, Potentiometer, TransistorFET, TransistorBipolar]
---
+++ CircuitSynthesis/PinDetection/pindetection.py
@@ -0,0 +1,300 @@
+import math
+import os
+import random
+import timeit
+import traceback
+from dataclasses import dataclass
+import json
+
+import cv2
+import numpy as np
+from scipy import ndimage
+from scipy.ndimage.measurements import label
+
+import PinDetection.image_manipulation as imgman
+import PinDetection.linedetection as ld
+from .match_pins import *
+
+@dataclass
+class Component:
+    component_img: np.ndarray
+    '''Grayscale component image as numpy array with dimensions (h, w). Background is white.'''
+
+    label_img: np.ndarray
+    '''Grayscale label image as numpy array with dimensions (h, w). Background is white.'''
+
+    label_offset: np.ndarray
+    '''Offset of the label image (top left corner to top left corner) as 1D numpy array with size 2: [x_off, y_off]'''
+
+    pins: Dict[str, Pin]
+    '''All pins in a dictionary where the key is the name of the pin.'''
+
+    type: str
+    '''String identifier of component type'''
+
+    _rotated: bool = False
+
+    def rotate(self, angle):
+        '''
+        Rotate all pins and images by an angle in degrees. Positive angle means counter-clockwise rotation.
+        This can only be done once per component, because otherwise the image would get bigger and bigger due to padding after rotation.
+        Label does not get rotated.
+        '''
+        if self._rotated:
+            raise RuntimeError("Component can only get rotated once! This is due to some reasons.")
+        
+        self._rotated = True
+
+        orig_size = np.array(self.component_img.shape[::-1])
+        self.component_img = ndimage.rotate(self.component_img, angle, cval=255)
+        new_size = np.array(self.component_img.shape[::-1])
+
+        self.label_offset = _rotate_point(self.label_offset - orig_size / 2 + np.array(self.label_img.shape[::-1]) / 2, -angle) + new_size / 2.0 - np.array(self.label_img.shape[::-1]) / 2
+
+        for pin in self.pins.values():
+            pin.position = _rotate_point((pin.position).astype(float) - orig_size / 2.0, -angle)
+            pin.position += new_size / 2.0
+            pin.direction = _rotate_point((pin.direction).astype(float), -angle)
+    
+    def flip(self, vert: bool = False, hor: bool = False):
+        '''
+        Flip the component vertically or horizontally.
+        "vert" and "hor" define if you flip it vertically, horizontally or both. Default is no flip.
+        Label does not get flipped.
+        '''
+        if hor:
+            self.component_img = self.component_img[:, ::-1]
+            self.label_offset[0] = self.component_img.shape[1] - self.label_offset[0] - self.label_img.shape[1]
+
+            for pin in self.pins.values():
+                pin.position[0] = self.component_img.shape[1] - pin.position[0]
+                pin.direction[0] *= -1
+        if vert:
+            self.component_img = self.component_img[::-1, :]
+            self.label_offset[1] = self.component_img.shape[0] - self.label_offset[1] - self.label_img.shape[0]
+
+            for pin in self.pins.values():
+                pin.position[1] = self.component_img.shape[0] - pin.position[1]
+                pin.direction[1] *= -1
+    
+    def scale(self, scale_factor: float):
+        '''Scale images and offsets by "scale_factor" (all axis with the same factor)'''
+        self.component_img = cv2.resize(self.component_img, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_CUBIC)
+        self.label_img = cv2.resize(self.label_img, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_CUBIC)
+        self.label_offset = (self.label_offset.astype(float) * scale_factor).astype(int)
+
+        for pin in self.pins.values():
+            pin.position = (pin.position.astype(float) * scale_factor).astype(int)
+
+@dataclass
+class UnloadedComponent:
+    '''Component where only the path to the images is stored. Use the ".load()" method to load the images and create a "Component" instance for further use.'''
+    _component_path: str
+    _label_path: str
+    _label_offset: np.ndarray
+    _pins: Dict[str, Pin]
+    _type: str
+
+    @staticmethod
+    def from_dict(map):
+        '''Create instance from dictionary from a *.json file.'''
+        pins = {name: Pin.from_dict(pin) for (name, pin) in map['pins'].items()}
+        return UnloadedComponent(map['component_path'], map['label_path'], np.array(map['label_offset']), pins, map['type'])
+
+    def load(self):
+        '''Load the images and create a "Component".'''
+        cmp_img = cv2.imread(self._component_path, cv2.IMREAD_UNCHANGED)
+        lbl_img = cv2.imread(self._label_path, cv2.IMREAD_UNCHANGED)
+
+        return Component(cmp_img, lbl_img, self._label_offset, self._pins, self._type)
+
+def import_components(data_path) -> Dict[str, List[UnloadedComponent]]:
+    with open(data_path, 'r') as f:
+        data_obj = json.load(f)
+        data_out: Dict[str, List[UnloadedComponent]] = {}
+
+        for type in data_obj:
+            data_out[type] = []
+            for cmp in data_obj[type]:
+                data_out[type].append(UnloadedComponent.from_dict(cmp))
+
+    return data_out
+
+def combine_images(a: np.ndarray, b: np.ndarray, off):
+    '''
+    Overlays two images (a and b) where image b is offset by values in "off".
+    off is an array with the x and y offset coordinates[x_off, y_off].
+    Images are overlayed by choosing the lower values per channel.
+    Resulting image is padded with white.
+    Used for combining component und label image.
+    '''
+    off = np.round(off).astype(int)
+    min_x = min(0, off[0])
+    min_y = min(0, off[1])
+
+    max_x = max(a.shape[1] - 1, b.shape[1] + off[0] - 1)
+    max_y = max(a.shape[0] - 1, b.shape[0] + off[1] - 1)
+
+    w = max_x - min_x + 1
+    h = max_y - min_y + 1
+
+    a_x = 0
+    a_y = 0
+    b_x = off[0]
+    b_y = off[1]
+
+    if off[0] < 0:
+        a_x = -off[0]
+        b_x = 0
+
+    if off[1] < 0:
+        a_y = -off[1]
+        b_y = 0
+
+    if len(a.shape) == 3 and len(b.shape) == 3:
+        a = np.pad(a, ((a_y, h - a.shape[0] - a_y), (a_x, w - a.shape[1] - a_x), (0, 0)), 'constant', constant_values=255)
+        b = np.pad(b, ((b_y, h - b.shape[0] - b_y), (b_x, w - b.shape[1] - b_x), (0, 0)), 'constant', constant_values=255)
+    else:
+        a = np.pad(a, ((a_y, h - a.shape[0] - a_y), (a_x, w - a.shape[1] - a_x)), 'constant', constant_values=255)
+        b = np.pad(b, ((b_y, h - b.shape[0] - b_y), (b_x, w - b.shape[1] - b_x)), 'constant', constant_values=255)
+
+    return np.minimum(a, b)
+
+def _rotate_point(point, angle):
+    c, s = np.cos(angle / 180 * np.pi), np.sin(angle / 180 * np.pi)
+    old_point = point.copy()
+    point[0] = old_point[0] * c - old_point[1] * s
+    point[1] = old_point[0] * s + old_point[1] * c
+
+    return point
+
+def _detect_pins(path, type):
+    img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+    orig_img = imgman.unify_color(img.copy())
+
+    if img is None:
+        print('Read empty image!')
+        return None, None, None, None
+
+    img = imgman.make_binary(img, 100)
+    img, cropp = imgman.filter(img)
+    orig_img = orig_img[cropp[0]:cropp[1], cropp[2]:cropp[3]]
+
+    if img is None:
+        print('Filtered empty image!')
+        return None, None, None, None
+
+    result = imgman.get_boundary(img)
+
+    if result is None:
+        print('No boundary!')
+        return None, None, None, None
+
+    minx, maxx, miny, maxy, centroid = result
+    img = img[miny:maxy, minx:maxx]
+    orig_img = orig_img[miny:maxy, minx:maxx]
+
+    sf = 1
+    if np.max(img.shape) < 100:
+        sf = 200 / np.max(img.shape)
+        img = cv2.resize(img, None, fx=sf, fy=sf, interpolation=cv2.INTER_AREA)
+        orig_img = cv2.resize(orig_img, None, fx=sf, fy=sf, interpolation=cv2.INTER_CUBIC)
+
+    kernel_size = math.ceil(min(img.shape[0], img.shape[1]) / 40.0) * 2 + 1
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, borderType=cv2.BORDER_CONSTANT, borderValue=0)
+
+    lines = ld.get_lines(img, 13, 10, 2, 17, 12)
+
+    for detector in ALL_DETECTORS:
+        if detector.match(type):
+            try:
+                pins = detector.get_pins(lines, centroid, np.array(img.shape))
+                return orig_img, pins, detector.NAME, {'bounds': (minx + cropp[2], maxx + cropp[3], miny + cropp[0], maxy + cropp[1]), 'sf': sf}
+            except Exception:
+                print("Detection not successful: " + traceback.format_exc())
+                return None, None, None, None
+    
+    print('No valid detector!')
+    return None, None, None, None
+
+def export_all(src_path, dest_path):
+    all_json = {}
+
+    for subdir in os.listdir(src_path):
+        folder = os.path.join(src_path, subdir)
+        if os.path.isdir(folder) and not folder.endswith('label'):
+            files = os.listdir(folder)
+
+            files = [os.path.join(folder, file) for file in files]
+            type = os.path.split(folder)[1]
+
+            for file in files:
+                component_img = None
+
+                try:
+                    # positioning includes cropping border and scaling factor (if it did get scaled)
+                    component_img, pins, name, positioning = _detect_pins(file, type)
+
+                    if component_img is not None:
+                        label_img = cv2.imread(os.path.join(folder + '_label', os.path.split(file)[1]), cv2.IMREAD_UNCHANGED)
+                        label_img = imgman.unify_color(label_img)
+                        label_img = cv2.resize(label_img, None, fx=positioning['sf'], fy=positioning['sf'], interpolation=cv2.INTER_AREA)
+                        
+                        lbl_minx, lbl_maxx, lbl_miny, lbl_maxy, _ = imgman.get_boundary(255 - label_img)
+                        label_img = label_img[lbl_miny:lbl_maxy, lbl_minx:lbl_maxx]
+
+                        label_off = np.array([-positioning['bounds'][0] * positioning['sf'] + lbl_minx, -positioning['bounds'][2] * positioning['sf'] + lbl_miny])
+
+                        cmp = Component(component_img, label_img, label_off, pins, type)
+                        
+                        if type in ['R_H', 'C_H', 'L_H']:
+                            cmp.type = cmp.type[0]
+                        elif type in ['R_V', 'C_V', 'L_V']:
+                            cmp.rotate(90 if random.random() > 0.5 else -90)
+                            cmp._rotated = False    # only allowed because it was a 90° rotation -> no padding
+                            cmp.type = cmp.type[0]
+                        elif type == 'POT':
+                            if cmp.pins['3'].direction[1] < 0:
+                                # pin direction is up -> flip vertically
+                                cmp.flip(vert=True)
+
+                except Exception:
+                    component_img = None
+                    print(traceback.format_exc())
+
+                if component_img is None:
+                    print('Error with file: ' + file)
+                    continue
+
+                print("Ok: " + file)
+
+                dest_folder = os.path.join(dest_path, cmp.type)
+
+                if not os.path.exists(dest_folder):
+                    os.makedirs(dest_folder)
+
+                if not os.path.exists(dest_folder + '_label'):
+                    os.makedirs(dest_folder + '_label')
+
+                cmp_path = os.path.join(dest_folder, os.path.split(file)[1])
+                lbl_path = os.path.join(dest_folder + '_label', os.path.split(file)[1])
+                cv2.imwrite(cmp_path, cmp.component_img)
+                cv2.imwrite(lbl_path, cmp.label_img)
+
+                if cmp.type not in all_json:
+                    all_json[cmp.type] = []
+                
+                all_json[cmp.type].append({
+                    'component_path': cmp_path,
+                    'label_path': lbl_path,
+                    'type': cmp.type,
+                    'label_offset': cmp.label_offset.tolist(),
+                    'pins': {pin_name: pin.to_dict() for (pin_name, pin) in cmp.pins.items()}
+                })
+
+    with open(os.path.join(dest_path, 'data.json'), 'x') as f:
+        json.dump(all_json, f)
+
+if __name__ == '__main__':
+    export_all('../Data/data_9_11/saved', './exported_data')
