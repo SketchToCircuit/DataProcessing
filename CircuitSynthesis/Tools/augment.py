ee56b7c172f67cab0f20b96f47599f970757b265
PatrickZ1 <36849297+PatrickZ1@users.noreply.github.com>
2021-12-07 14:41:50 +0100

    remove lines over components

--- CircuitSynthesis/Tools/augment.py
+++ CircuitSynthesis/Tools/augment.py
@@ -6,7 +6,4 @@ ERODE_PROB = 0.8
 MAX_ERODE_SIZE = 3
 
 def augment_cmp_img(img):
-    if random.random() < 0.5:
-        img = cv2.erode(img, np.ones((random.randint(1, MAX_ERODE_SIZE), random.randint(1, MAX_ERODE_SIZE))))
-    
     return img
