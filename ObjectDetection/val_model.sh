7481fa4ee2d509878665e9a6a93b2ed63fac83d9
hauminix <tim.matteo.brunner@gmail.com>
2021-12-10 20:47:04 +0100

    v5 with all labels

--- ObjectDetection/val_model.sh
+++ ObjectDetection/val_model.sh
@@ -3,7 +3,7 @@ eval "$(conda shell.bash hook)"
 conda activate neural
 export CUDA_VISIBLE_DEVICES=-1
 python model_main_tf2.py \
---pipeline_config_path=./models/ssd_resnet101_640/v4/pipeline.config \
---model_dir=./models/ssd_resnet101_640/v4/ \
---checkpoint_dir=./models/ssd_resnet101_640/v4/ \
+--pipeline_config_path=./models/ssd_resnet101_640/v5/pipeline.config \
+--model_dir=./models/ssd_resnet101_640/v5/ \
+--checkpoint_dir=./models/ssd_resnet101_640/v5/ \
 --sample_1_of_n_eval_examples=1
