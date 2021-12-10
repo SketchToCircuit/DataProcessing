889822d6180c56b61fe7b7de24cc14b2dc431d76
hauminix <tim.matteo.brunner@gmail.com>
2021-12-10 14:49:49 +0100

    v4 with new anchors

--- ObjectDetection/train_model.sh
+++ ObjectDetection/train_model.sh
@@ -2,7 +2,7 @@
 eval "$(conda shell.bash hook)"
 conda activate neural
 python model_main_tf2.py \
---pipeline_config_path=./models/ssd_resnet101_640/v3/pipeline.config \
---model_dir=./models/ssd_resnet101_640/v3/ \
+--pipeline_config_path=./models/ssd_resnet101_640/v4/pipeline.config \
+--model_dir=./models/ssd_resnet101_640/v4/ \
 --checkpoint_every_n=100 \
 --alsologtostderr
