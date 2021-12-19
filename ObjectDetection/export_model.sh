162889944211bca4fe3b12e2084765222e14377e
hauminix <tim.matteo.brunner@gmail.com>
2021-12-19 13:16:40 +0100

    v8 model

--- ObjectDetection/export_model.sh
+++ ObjectDetection/export_model.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate neural
+export CUDA_VISIBLE_DEVICES=-1
 python exporter_main_v2.py \
---pipeline_config_path=./models/ssd_resnet101_640/v6/pipeline.config \
---trained_checkpoint_dir=./models/ssd_resnet101_640/v6/ \
---output_directory=./exported_models/ssd_resnet101_640_v6/ \
+--pipeline_config_path=./models/ssd_resnet101_640/v8/pipeline.config \
+--trained_checkpoint_dir=./models/ssd_resnet101_640/v8/best_ckps \
+--output_directory=./exported_models/ssd_resnet101_640_v8/ \
 --input_type=image_tensor
