76e46e0248bc47013c70bcdc1470a293b8741343
hauminix <tim.matteo.brunner@gmail.com>
2022-01-27 14:54:53 +0100

    new pin detection export

--- CircuitSynthesis/export_pins.py
+++ CircuitSynthesis/export_pins.py
@@ -1,3 +1,3 @@
 from Tools.PinDetection.pindetection import export_all
 
-export_all('./DataProcessing/ExportData/Data', './DataProcessing/pindetection_data_new')
+export_all('./DataProcessing/ExportData/Data', './DataProcessing/pindetection_data')
